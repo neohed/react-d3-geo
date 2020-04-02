c3765ae081c127307a39018fd37a9ce39a113eb8
neohed <35173474+neohed@users.noreply.github.com>
2020-04-02 10:48:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import Map01 from './Map01'
+import WorldMap from "./D3InAction/WorldMap";
+import WorldMap_topojson from "./D3InAction/WorldMap_topojson";
 
 function App() {
-  return (
-    <div className="App">
-      <Map01/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Map01</h1>
+            <Map01/>
+            <h1>WorldMap</h1>
+            <WorldMap/>
+            <h1>WorldMap_topojson</h1>
+            <WorldMap_topojson/>
+        </div>
+    );
 }
 
 export default App;
